5e95095e7d395883b7ea17d2dd0292cc3b091a3a
Jose Maria Garcia <josemaria.alkala@gmail.com>
2025-03-05 23:44:52 +0100

    more parameters added

--- src/stage/stage.nim
+++ src/stage/stage.nim
@@ -1,3 +1,4 @@
+# https://konvajs.org/api/Konva.Stage.html
 import jsffi
 import ../layer/layer
 
@@ -8,11 +9,26 @@ type
     add*: proc(layer:LayerObj) {.closure.}   
 
 
-proc newStage*(options:JsObject): StageObj {. importcpp: "new Konva.Stage(@)" .}
+proc newStage*(config:JsObject): StageObj {. importcpp: "new Konva.Stage(@)" .}
 
-proc newStage*(container:string; width,height:int): StageObj =
-  var options = newJsObject()
-  options["container"] = container.cstring
-  options["width"]     = width.cint
-  options["height"]    = height.cint
-  return newStage(options)
+proc newStage*(container:string; width,height:int; 
+               x:int = 0; y:int = 0; 
+               scale:int = 1; scaleX:int = 1; scaleY:int = 1): StageObj =
+  var config = newJsObject()
+  config["container"] = container.cstring
+  config["width"]     = width.cint
+  config["height"]    = height.cint
+  
+  if x>=0:
+    config["x"]    = x.cint
+  if y>=0:
+    config["y"]    = y.cint
+
+  if scale != 1:
+    config["scale"]    = scale.cint    
+  if scaleX != 1:
+    config["scaleX"]    = scaleX.cint
+  if scaleY != 1:
+    config["scaleY"]    = scaleY.cint
+
+  return newStage(config)

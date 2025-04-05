106bca69d4f0552b59399d6dc5243d5b1c4b8117
Jose Maria Garcia <josemaria.alkala@gmail.com>
2025-04-05 15:35:26 +0200

    circle example

---
+++ examples/tut01_circle.nim
@@ -0,0 +1,19 @@
+import konva
+import std/dom, jsconsole
+
+# https://konvajs.org/docs/overview.html#how-does-it-work
+
+proc main =
+  var stage = newStage("container", 500, 500)
+  var layer = newLayer()
+  var circle = newCircle(Circle(x: (stage.width()/2).float, 
+                                y: (stage.height()/2), 
+                                radius: 70,
+                                fill: "red", 
+                                stroke: "black", 
+                                strokeWidth: 4) )
+  layer.add(circle)  # add the shape to the layer
+
+  stage.add(layer)   # add the layer to the stage
+
+main()
